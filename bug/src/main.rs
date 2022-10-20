136b780cbf761fed41b90bbee4d23386fc7a9306
Benjamin Bouvier <public@benj.me>
2022-10-20 23:54:03 +0200

    simplify: no need for specific metadata + allow running from top-level dir

--- bug/src/main.rs
+++ bug/src/main.rs
@@ -1,8 +1,5 @@
 fn main() {
-    #[derive(serde::Deserialize)]
-    struct Metadata {}
-
-    if let Err(err) = cargo_toml::Manifest::<Metadata>::from_path_with_metadata("../foo/Cargo.toml") {
+    if let Err(err) = cargo_toml::Manifest::from_path("./foo/Cargo.toml") {
         eprintln!("error when reading the manifest: {}", err);
     } else {
         eprintln!("allright!");
